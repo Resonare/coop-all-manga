9d331e73bb6a41e423984123b9df869465810249
Resonare <ioepochoi@gmail.com>
2025-02-28 19:44:43 +1000

    created parser app

--- parser/app.js
+++ parser/app.js
@@ -1,1 +1,20 @@
-console.log("initial test")
+import express from "express";
+
+const PORT = 3000;
+
+const app = express();
+
+app.get("/", async (req, res) => {
+  try {
+    const response = await fetch(
+      "https://lib.social/api/faq/questions"
+    );
+
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Ошибка запроса" });
+  }
+});
+
+app.listen(PORT, () => console.log(`Паресер запущен на порту ${PORT}`));
