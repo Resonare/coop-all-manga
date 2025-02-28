39d51f07d9df25489018e612eb609076e0b26926
Resonare <ioepochoi@gmail.com>
2025-02-28 20:19:23 +1000

    made some tests

--- parser/app.js
+++ parser/app.js
@@ -1,13 +1,17 @@
 import express from "express";
+import fs from 'fs';
 
 const PORT = 3000;
 
 const app = express();
 
+// const data = JSON.parse(fs.readFileSync('./page.json', 'utf-8'));
+// console.log(data["data"].length);
+
 app.get("/", async (req, res) => {
   try {
     const response = await fetch(
-      "https://lib.social/api/faq/questions"
+      "https://api.lib.social/api/manga?fields[]=rate&fields[]=rate_avg&fields[]=userBookmark&site_id[]=1&page=8"
     );
 
     const data = await response.json();
