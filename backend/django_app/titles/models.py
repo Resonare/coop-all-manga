b60cc16c02af42387f49a606348202686491cd28
Serzm7988 <119091549+serzm7988@users.noreply.github.com>
2025-03-10 22:04:18 +1000

    Fix models and gitignore venv

--- backend/django_app/titles/models.py
+++ backend/django_app/titles/models.py
@@ -1,17 +1,15 @@
 from django.db import models
 
 
-class Titles(models.Model):
-    name = models.CharField(max_length=50)
-    year = models.IntegerField()
-    status = models.CharField(max_length=50)
-    type = models.CharField(max_length=50)
-    rating = models.FloatField()
-    thumbnail = models.CharField(max_length=50)
+class Title(models.Model):
+    """
+    Модель данных для тайтла
+    """
+    pass
 
-    class Meta:
-        verbose_name_plural = 'Тайтлы'
-        verbose_name = 'Тайтл'
 
-    def __str__(self):
-        return self.name
+class Сhapter(models.Model):
+    """
+    Модель данных для глав тайтлов
+    """
+    pass
