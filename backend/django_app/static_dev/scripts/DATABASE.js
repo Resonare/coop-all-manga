993081e8821054af94af5dde7bb8d22001789db1
Serzm7988 <119091549+serzm7988@users.noreply.github.com>
2025-03-10 18:13:57 +1000

    Add templates and static_dev

---
+++ backend/django_app/static_dev/scripts/DATABASE.js
@@ -0,0 +1,135 @@
+const mangaData = {
+    "manga1": { 
+        title: "Манга 1", 
+        Mangalibchapters: [
+            {
+                chapter_title: "Chapter 1",
+                translators: [
+                    { name: "Team A", link: "testlink1" },
+                    { name: "Team B", link: "testlink2" }
+                ]
+            }
+            {
+                chapter_title: "Chapter 2",
+                translators: [
+                    { name: "Team A", link: "testlink1" },
+                    { name: "Team B", link: "testlink2" }
+                ]
+            }
+            {
+                chapter_title: "Chapter 3",
+                translators: [
+                    { name: "Team A", link: "testlink1" },
+                    { name: "Team B", link: "testlink2" }
+                ]
+            }
+            {
+                chapter_title: "Chapter 4",
+                translators: [
+                    { name: "Team A", link: "testlink1" },
+                    { name: "Team B", link: "testlink2" }
+                ]
+            }
+        ], 
+        Remangachapters: [
+            {
+                chapter_title: "Chapter 1",
+                translators: [
+                    { name: "Team A", link: "testlink1" },
+                    { name: "Team B", link: "testlink2" }
+                ]
+            }
+            {
+                chapter_title: "Chapter 2",
+                translators: [
+                    { name: "Team A", link: "testlink1" },
+                    { name: "Team B", link: "testlink2" }
+                ]
+            }
+            {
+                chapter_title: "Chapter 3",
+                translators: [
+                    { name: "Team A", link: "testlink1" },
+                    { name: "Team B", link: "testlink2" }
+                ]
+            }
+        ]
+    },
+    "manga2": { 
+        title: "Манга 2", 
+        Mangalibchapters: [
+            {
+                chapter_title: "Chapter 1",
+                translators: [
+                    { name: "Team A", link: "testlink1" },
+                    { name: "Team B", link: "testlink2" }
+                ]
+            },
+            {
+                chapter_title: "Chapter 2",
+                translators: [
+                    { name: "Team A", link: "testlink1" },
+                    { name: "Team B", link: "testlink2" }
+                ]
+            },
+            {
+                chapter_title: "Chapter 3",
+                translators: [
+                    { name: "Team A", link: "testlink1" },
+                    { name: "Team B", link: "testlink2" }
+                ]
+            },
+            {
+                chapter_title: "Chapter 4",
+                translators: [
+                    { name: "Team A", link: "testlink1" },
+                    { name: "Team B", link: "testlink2" }
+                ]
+            },
+            {
+                chapter_title: "Chapter 4",
+                translators: [
+                    { name: "Team A", link: "testlink1" },
+                    { name: "Team B", link: "testlink2" }
+                ]
+            }
+        ], 
+        Remangachapters: [
+            {
+                chapter_title: "Chapter 1",
+                translators: [
+                    { name: "Team A", link: "testlink1" },
+                    { name: "Team B", link: "testlink2" }
+                ]
+            },
+            {
+                chapter_title: "Chapter 2",
+                translators: [
+                    { name: "Team A", link: "testlink1" },
+                    { name: "Team B", link: "testlink2" }
+                ]
+            },
+            {
+                chapter_title: "Chapter 3",
+                translators: [
+                    { name: "Team A", link: "testlink1" },
+                    { name: "Team B", link: "testlink2" }
+                ]
+            },
+            {
+                chapter_title: "Chapter 2",
+                translators: [
+                    { name: "Team A", link: "testlink1" },
+                    { name: "Team B", link: "testlink2" }
+                ]
+            },
+            {
+                chapter_title: "Chapter 3",
+                translators: [
+                    { name: "Team A", link: "testlink1" },
+                    { name: "Team B", link: "testlink2" }
+                ]
+            }
+        
+    ]   }
+}
