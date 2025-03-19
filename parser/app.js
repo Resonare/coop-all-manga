dd943148f2ca8ea211ce2ae5dd179d5952f3483a
Resonare <47446377+Resonare@users.noreply.github.com>
2025-03-19 17:47:12 +1000

    added all fields + changed routing

--- parser/app.js
+++ parser/app.js
@@ -3,14 +3,14 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 const PORT = 3000;
-const HOW_MANY_PAGES_TO_PARSE = 3; // Уменьши для теста
+const HOW_MANY_PAGES_TO_PARSE = 50; // Уменьши для теста
 
 const app = express();
 
 const fetchListPage = async (pageNumber) => {
   try {
     console.log(`Парсим страницу: ${pageNumber}`);
-    
+
     const response = await fetch(
       `https://api.lib.social/api/manga?site_id[]=1&page=${pageNumber}`
     );
@@ -18,14 +18,21 @@ const fetchListPage = async (pageNumber) => {
     if (!response.ok) throw new Error(`Ошибка запроса: ${response.status}`);
 
     const mangaList = (await response.json())["data"];
+    const resList = [];
 
     for (const manga of mangaList) {
       const mangaObject = {
         name: manga["name"],
+        year: manga["releaseDateString"].substring(0, manga["releaseDateString"].length),
+        status: manga["status"]["label"],
+        type: manga["type"]["label"],
+        rating: manga["rating"]["average"],
         mangalib_url: manga["slug_url"],
         cover: manga["cover"]["default"],
         thumbnail: manga["cover"]["thumbnail"],
       };
+
+      resList.push(mangaObject);
 
       console.log(`Сохраняем в БД: ${mangaObject.name}`);
 
@@ -36,14 +43,14 @@ const fetchListPage = async (pageNumber) => {
       });
     }
 
-    return mangaList;
+    return resList;
   } catch (error) {
     console.error("Ошибка при парсинге:", error);
     return [];
   }
 };
 
-app.get("/", async (req, res) => {
+app.get("/parse", async (req, res) => {
   const startTime = new Date();
 
   let finalList = [];
@@ -56,6 +63,19 @@ app.get("/", async (req, res) => {
 
   console.log(`Времени затрачено: ${new Date() - startTime} мс`);
   res.json(finalList);
+});
+
+app.get("/get", async (req, res) => {
+  const startTime = new Date();
+
+  console.log(`Пытаемся получить список манги`);
+
+  const manga = await prisma.manga.findMany();
+
+  await prisma.$disconnect(); // Закрываем соединение с БД
+
+  console.log(`Времени затрачено: ${new Date() - startTime} мс`);
+  res.json(manga);
 });
 
 app.listen(PORT, () => console.log(`Парсер запущен на порту ${PORT}`));
